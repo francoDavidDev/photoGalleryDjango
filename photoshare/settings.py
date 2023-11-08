63c16a1bf3cdd093b0b24588b987eadabfe4860b
franco66 <david_franco@outlook.com>
2023-11-08 11:56:08 -0300

    urlril

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -146,5 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-CSRF_TRUSTED_ORIGINS = ["http://*","https://galleryphotodjango.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["http://*","https://galleryphotodjango.up.railway.app"]
 
